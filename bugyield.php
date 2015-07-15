5fdee36b69643a78a0aaf668d0e773ed4b67f407
André Kallehauge <andrekallehauge@gmail.com>
2015-07-15 10:43:15 +0200

    Remove unused classes in the bugyield init file

--- bugyield.php
+++ bugyield.php
@@ -1,8 +1,6 @@
 <?php
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Command\Command;
 
 //Setup Symfony classloader and components
 require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
