04ebd445c7e746147b5c1661f7f3800f53c5e2a3
André Kallehauge <andrekallehauge@gmail.com>
2015-07-15 13:36:24 +0200

    Use Composers autoloader instead of Symfony ClassLoader

--- bugyield.php
+++ bugyield.php
@@ -1,17 +1,6 @@
 <?php
 
-use Symfony\Component\ClassLoader\ClassLoader;
-
-// Composer autoloader (PSR-4).
 require 'vendor/autoload.php';
-
-// Setup Symfony classloader and components (PSR-0).
-$loader = new ClassLoader();
-$loader->setUseIncludePath(true);
-$loader->addPrefixes(array(
-  'BugYield' => __DIR__.'/lib/',
-));
-$loader->register();
 
 //Load FogBugz API library
 require_once 'vendor/kasperg/fogbugz-php-library/src/fogbugz_init.php';
